55f3415f9f3523583bbeb03c6d3959ef0e32c213
Dani4kor <dani4kor@gmail.com>
2016-11-06 16:34:46 +0200

    Update app
    add json response

--- cryptomessage/server.py
+++ cryptomessage/server.py
@@ -8,10 +8,14 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import tornado.escape
+
 
 from algorithms import shuffle
 
+
+import json
+from tornado.escape import json_decode
+from tornado import gen
 from tornado.options import define, options, parse_command_line
 
 
@@ -20,31 +24,30 @@ define("port", default=8888, help="run on the given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", message='HELLO WORLD')
+        self.render("index.html", message='')
 
-    def post(self):
-        message = {
-            "id": str(uuid.uuid4()),
-            "uncrypt-message": self.get_argument("uncrypt-message"),
-        }
-        self.render("index.html", message=shuffle.shuffle_string(message["uncrypt-message"]))
 
 class UpdateHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", message='Hello Update')
+        example_response = {'key': None , 'value': None }
+        self.write(json.dumps(example_response))
+
+    def post(self):
+        json_obj = json_decode(self.request.body)
+        response_to_send = {'newkey': json_obj['message']}
+        self.write(json.dumps(response_to_send))
+
 
 def main():
     parse_command_line()
     app = tornado.web.Application(
         [
             (r"/", MainHandler),
-            (r"/update", UpdateHandler),
+            (r"/update/", UpdateHandler),
         ],
 
-        cookie_secret="12345",
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
-        xsrf_cookies=True,
     )
     app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
