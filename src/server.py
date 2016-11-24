18166121ab1c15fb222c7831231020750389eac9
Dani4kor <dani4kor@gmail.com>
2016-11-24 22:33:14 +0200

    add async httpclient

--- src/server.py
+++ src/server.py
@@ -9,6 +9,7 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from tornado import httpclient
 from tornado.ioloop import IOLoop
 
 import multiprocessing
@@ -20,21 +21,23 @@ from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
 from tornado.options import define, options, parse_command_line
 from concurrent.futures import ThreadPoolExecutor
+from urlparse import urljoin, urldefrag
+
+
 
 define("port", default=8888, help="run on the given port", type=int)
 
+
+
+@gen.coroutine
+def waits():
+    yield gen.Task(IOLoop.instance().add_timeout, time.time() + 1)
 
 class VersionHandler(tornado.web.RequestHandler):
     def get(self):
         response = {'version': '0.0.1',
                     'last_build': date.today().isoformat()}
         self.write(json.dumps(response))
-
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html", message='')
-
 
 class ParseHandler(tornado.web.RequestHandler):
     executor = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
@@ -44,17 +47,31 @@ class ParseHandler(tornado.web.RequestHandler):
         response = {'img': json_obj["message"]}
         self.write(json.dumps(response))
 
-
 @gen.coroutine
-def waits():
-    yield gen.Task(IOLoop.instance().add_timeout, time.time() + 1)
+def get_links_from_url(url):
+
+    response = yield httpclient.AsyncHTTPClient().fetch(url)
+    print('fetched %s' % url)
+
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        url = 'http://spacetelescope.org/images/'
+        get_links_from_url(url)
+        self.render("index.html")
+
+
+
+
+
 
 
 def main():
     parse_command_line()
     app = tornado.web.Application(
         [
-            (r"/", MainHandler),
+            (r"/?", MainHandler),
             (r"/version/?", VersionHandler),
             (r"/parse/?", ParseHandler),
         ],
