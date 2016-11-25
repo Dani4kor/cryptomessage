b8e500565c351d65b71d3a54e6c23c345642ab2d
Dani4kor <dani4kor@gmail.com>
2016-11-25 16:15:13 +0200

    update gist

--- src/server.py
+++ src/server.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 
 import os
-import uuid
-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 from tornado import httpclient
 from tornado.ioloop import IOLoop
-
-import multiprocessing
-import json
-
 from datetime import date
+
+import json
 from tornado.escape import json_decode
 from tornado import gen
 from tornado.options import define, options, parse_command_line
 from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
 import time
-from datetime import timedelta
-import pprint
+
 
 define("port", default=8888, help="run on the given port", type=int)
 
@@ -39,21 +34,15 @@ class VersionHandler(tornado.web.RequestHandler):
 
 
 class ParseHandler(tornado.web.RequestHandler):
-    executor = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
-
     @gen.coroutine
     def post(self):
         url = self.get_body_argument('message')
-
         if url.startswith('http://www.'):
             url = 'http://' + url[len('http://www.'):]
         elif url.startswith('www.'):
             url = 'http://' + url[len('www.'):]
         elif not url.startswith('http://'):
             url = 'http://' + url
-
-        print url
-
         images = yield get_img(url)
         self.render("index.html", image_message=images)
 
